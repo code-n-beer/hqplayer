7085055fd2b29e3d207fa9b94342a379503166f8
Matias Juntunen <matias.juntunen@gmail.com>
2017-04-15 14:34:00 +0300

    Rework playlist stuff.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mpv = require("node-mpv");
-const Promise = require("promise");
+const Promise = require("bluebird");
+const { getInfo } = require("ytdl-getinfo");
 
 const app = express();
 
@@ -10,50 +11,46 @@ app.locals.player = new mpv({
     verbose: true
 });
 
+app.locals.queue = [];
+app.locals.current = null;
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
 app.get("/now-playing", (req, res) => {
-    req.app.locals.player.getProperty("media-title")
-        .then(title => res.json({
-            title: title
-        }));
+    res.json(req.app.locals.current);
 });
 
-app.get("/playlist", (req, res) => {
-    req.app.locals.player.getProperty("playlist/count")
-        .then(count => {
-            let entries = [];
-            console.log(`getting ${count} entries`);
-
-            for (let i = 0; i < count; i++) {
-                const entryName = `playlist/${i}/filename`;
-                console.log(entryName);
-                entries.push(player.getProperty(entryName));
-            }
-
-            console.log(entries);
-            return Promise.all(entries);
-        })
-        .then(playlist => res.json({
-            entries: playlist
-        }));
+app.get("/queue", (req, res) => {
+    res.json(req.app.locals.queue);
 });
 
-app.post("/playlist", (req, res) => {
+app.post("/queue", (req, res) => {
     const url = req.body.url.replace(/\\/g, "");
-    req.app.locals.player.append(url, "append-play");
 
-    req.app.locals.player.getProperty("playlist/count")
-        .then(count => {
-            res.json({
-                status: {
-                    position: count
-                },
-                url: url
-            });
+    getInfo(url).then((info) => {
+        const entry = {
+            title: info.items[0].title,
+            url: url
+        };
+
+        let status;
+
+        if (req.app.locals.current) {
+            req.app.locals.queue.push(entry);
+            status = "queued";
+        } else {
+            req.app.locals.current = entry;
+            req.app.locals.player.loadStream(entry.url);
+            status = "playing";
+        }
+
+        res.json({
+            status,
+            info: entry
         });
+    });
 });
 
 app.post("/play", (req, res) => {
@@ -73,11 +70,29 @@ app.post("/pause", (req, res) => {
 });
 
 app.post("/next", (req, res) => {
-    req.app.locals.player.next();
+    const entry = req.app.locals.queue.shift();
+
+    if (!entry) {
+        res.json({
+            status: "idle"
+        });
+
+        return;
+    }
+
+    req.app.locals.current = entry;
+    req.app.locals.player.loadStream(entry.url);
 
     res.json({
-        status: "next"
+        status: "playing",
+        info: entry
     });
 });
 
-app.listen(3000, () => console.log("Running..."));
+app.listen(3000, () => {
+    console.log("Running...");
+
+    app.locals.player.on("stopped", () => {
+        app.locals.current = null;
+    });
+});
